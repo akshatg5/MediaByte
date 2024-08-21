3d25ce67551922f93a785f3867c62ef958f9471d
akshatg5 <akshatgirdhar5@gmail.com>
2024-08-21 15:10:23 +0530

    fix : caching videos

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,15 +1,27 @@
-import { NextRequest,NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client"
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-    try {
-        const videos = await prisma.video.findMany({})
-        return NextResponse.json(videos)
-    } catch (error) {
-        return NextResponse.json({error : "Error fetching the videos."},{status : 500})
-    } finally {
-        await prisma.$disconnect()
-    }
+  try {
+    const videos = await prisma.video.findMany({});
+    const response = NextResponse.json(videos);
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Surrogate-Control", "no-store");
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error fetching the videos." },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
